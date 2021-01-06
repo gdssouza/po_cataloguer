a2ea01c7fbfb29c32027eae9db44f546896c19f9
Gustavo Souza <gdssouza@id.uff.br>
2021-01-06 15:13:53 -0300

    Removendo exemplo

--- catalogador.py
+++ catalogador.py
@@ -21,18 +21,10 @@ config = open('config.txt')
 email = config.readline().strip('\n')   
 password = config.readline().strip('\n')
 
-# variaveis
-mode = 'PRACTICE'
-start = '2020/12/01'
-end = '2020/12/18'
-timeframe = 60*15
-par = 'EURUSD'
-
-#start = input("Insira a data de início YYYY/MM/DD")
-#start = input("Insira a data de término YYYY/MM/DD")
-#timeframe = int(input("Insira o timeframe em minutos: "))*60
-#par = input("Insira o ativo: ")
-#print()
+start = input("Insira a data de início YYYY/MM/DD :/> ")
+end = input("Insira a data de término YYYY/MM/DD  :/> ")
+timeframe = int(input("Insira o timeframe em minutos :/> "))*60
+par = input("Insira o ativo :/> ")
 
 api = catalogador(email,password)
 df = api.ler_candles(par, timeframe, start, end)
